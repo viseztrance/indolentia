76db9e231e7fa577a4d028173ed8bd83b2679124
Daniel Mircea <daniel@viseztrance.com>
2013-01-14 21:46:56 +0200

    Start with homeworld selected

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,17 +7,12 @@ $(function() {
         MemoryStore.save("game", game);
     }
     game.render();
-    game.map.center(game.galaxy.origin[0], game.galaxy.origin[1]);
 
     UI.getInstance().set({
         info: $("aside.info")
     });
 
-    // $("#zoom-in").click(function() {
-    //     map.zoomIn(1.05);
-    // });
-    // $("#zoom-out").click(function() {
-    //     map.zoomOut(1.05);
-    // });
-
+    game.map.center(game.galaxy.origin[0], game.galaxy.origin[1]);
+    var homeworld = game.currentPlayer.ownedStars[0];
+    $.proxy(homeworld.events.click, homeworld)();
 });
